3a8436ca74192c5f00fca595de53e9e49e2e38f5
James Cockfield <jamescockfield10@gmail.com>
2022-02-17 21:58:40 +0000

    update header

--- resources/views/layouts/includes/nav-head.blade.php
+++ resources/views/layouts/includes/nav-head.blade.php
@@ -1,4 +1,9 @@
 <div class="nav-header">
+    <a href="{{ route('goto') }}" class="brand-logo">
+        <img class="logo-abbr" src="{{ asset('images/logo.png') }}" width="50" height="50">
+        <span class="brand-title"> {{ config('app.name', 'Laravel') }}</span>
+    </a>
+
     <div class="nav-control">
         <div class="hamburger">
             <span class="line"></span><span class="line"></span><span class="line"></span>
